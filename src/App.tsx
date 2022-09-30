d7b342cb5f27f028663aaf41df29555e33517d57
chaudhuree <chaudhuree00@gmail.com>
2022-09-30 15:56:51 +0600

    array of type of an interface..[todos,setTodos]

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 import Basics from "./basics";
 import { FC, useState } from "react";
 import InputField from "./components/InputField";
+import Todo from './models/models'
 
 const App: FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos,setTodos]=useState<Todo[]>([])
   return (
     <div className="App">
       {/*<Basics></Basics>*/}
